af76c749aab39f839987e876a0382773fa59f785
Bastien <bzg@gnu.org>
2020-03-02 08:33:58 +0100

    Add some docstrings

--- src/core.clj
+++ src/core.clj
@@ -38,14 +38,19 @@
 
 (def http-get-params {:cookie-policy :standard})
 
-(defn sill-contributors-to-json []
+(defn sill-contributors-to-json
+  "Spit sill-contributors.json from `sill-contributors-url`."
+  []
   (spit "sill-contributors.json"
         (json/generate-string
          (try (semantic-csv/slurp-csv sill-contributors-url)
               (catch Exception e
                 (println "Cannot reach SILL csv URL"))))))
 
-(defn get-sill []
+(defn get-sill
+  "Get SILL from `sill-url`.
+  This is used before wikidata data have been fetched."
+  []
   (map #(clojure.set/rename-keys
          (select-keys % (keys sill-mapping))
          sill-mapping)
@@ -53,7 +58,9 @@
             (catch Exception e
               (println "Cannot reach SILL csv URL")))))
 
-(defn wd-get-data [entity]
+(defn wd-get-data
+  "For a wikidata entity, fetch data needed for the SILL."
+  [entity]
   (when (not-empty entity)
     (-> (try (http/get (str wikidata-base-url entity ".json")
                        http-get-params)
@@ -65,7 +72,9 @@
         (as-> e ((keyword entity) e)) ;; kentity
         (select-keys [:claims :descriptions]))))
 
-(defn wc-get-image-url-from-wm-filename [f]
+(defn wc-get-image-url-from-wm-filename
+  "From a filename f, get the image url."
+  [f]
   (if-let [src (try (:body (http/get
                             (str wikidata-base-image-url
                                  (codec/url-encode f "UTF-8"))
@@ -81,15 +90,19 @@
            first
            :content))))
 
-(defn wd-get-first-value [k claims]
-  (:value (:datavalue (:mainsnak (first (k claims))))))
+(defn wd-get-first-value
+  "Get the first value of list of claims for property p."
+  [p claims]
+  (:value (:datavalue (:mainsnak (first (p claims))))))
 
 ;; Other properties to consider:
 ;; - P178: developer
 ;; - P275: license
 ;; - P18: image
 ;; - P306: operating system (linux Q388, macosx Q14116, windows Q1406)
-(defn sill-plus-wikidata []
+(defn sill-plus-wikidata
+  "Spit sill.json by adding wikidata data."
+  []
   (for [entry (get-sill)]
     (-> (if-let [w (not-empty (:w entry))]
           (let [data       (wd-get-data w)
